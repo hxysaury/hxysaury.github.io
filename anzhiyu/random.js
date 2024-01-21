40d60e8013e68912791c5f3e68b401c2638599ec
Vocal <saury@666.com>
2024-01-21 20:25:04 +0800

    Site updated: 2024-01-21 20:25:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
 var posts=["2024/01/21/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
