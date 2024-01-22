d8406021c41621f60b829721aa52d4914caf678b
Vocal <saury@666.com>
2024-01-22 14:36:04 +0800

    Site updated: 2024-01-22 14:36:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/11/phpMyAdmin利用/"];function toRandomPost(){
+var posts=["2023/11/22/linux提权姿势/","2023/11/20/phpMyAdmin利用/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
