3c127e08dcc423e91b4b8cda7b70bddcd2006882
Vocal <saury@666.com>
2024-01-22 17:59:58 +0800

    Site updated: 2024-01-22 17:59:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/24/Windows提权姿势/","2023/11/22/linux提权姿势/","2023/11/20/phpMyAdmin利用/","2023/11/29/sqlmap/"];function toRandomPost(){
+var posts=["2023/11/28/WAF攻防/","2023/11/22/MYSQL提权/","2023/11/24/Windows提权姿势/","2023/11/22/linux提权姿势/","2023/11/29/nc/","2023/11/20/phpMyAdmin利用/","2023/11/29/sqlmap/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
