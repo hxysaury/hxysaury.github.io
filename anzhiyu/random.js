a4cc316546517a889f5c4f11bc0ce5218dbd0c59
bypass <bypass@qq.com>
2025-02-24 13:49:13 +0800

    Site updated: 2025-02-24 13:49:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/hello-world/","2025/02/24/shell/shell基础知识/"];function toRandomPost(){
+var posts=["2025/02/24/hello-world/","2025/02/24/shell/shell基础知识/","2025/02/23/shell/vim使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
