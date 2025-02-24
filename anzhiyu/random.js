c0a7d0e0553d3c9e48281bade851411cd474c07f
bypass <bypass@qq.com>
2025-02-24 17:52:22 +0800

    Site updated: 2025-02-24 17:52:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/Linux/shell/shell基础知识/","2025/02/23/Linux/shell/vim使用/"];function toRandomPost(){
+var posts=["2025/02/24/Linux/shell基础知识/","2025/02/23/Linux/vim使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
